5b1744ee3c5084e7f9d3fde3c8a699e1a42fb53c
paidaxing <122286594+paidaxing1234@users.noreply.github.com>
2025-12-01 10:05:50 +0800

    Add files via upload
    示例

---
+++ HftBacktest-main/hft_backtest/strategy.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+from hft_backtest import EventEngine, Data, Order, OrderState, Component
+
+class Strategy(Component, ABC):
+    """
+    策略抽象基类
+    on_data方法需要被子类实现
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def send_order(self, order: Order):
+        """发送订单到事件引擎"""
+        assert order.state == OrderState.CREATED or order.is_cancel
+        order.state = OrderState.SUBMITTED
+        self.event_engine.put(order)
+
+    @abstractmethod
+    def on_data(self, data: Data):
+        """处理数据事件"""
+        pass
+
+    def on_order(self, order: Order):
+        """处理订单事件，子类可选实现"""
+        pass
+
+    def start(self, engine: EventEngine):
+        self.event_engine = engine
+        engine.register(Data, self.on_data)
+        engine.register(Order, self.on_order)
+
+    def stop(self):
+        pass
